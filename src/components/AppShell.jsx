e03579a527eee0cc82e0b35addd6508ede3df40c
aliuntravelsrl-hash <aliuntravelsrl@gmail.com>
2026-07-05 16:51:39 -0400

    feat: ruta payment gateway page

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -56,6 +56,7 @@ export function AppShell() {
               <Route path="admin/hotels" element={<AdminHotelsPage />} />
               <Route path="admin/excursions" element={<AdminExcursionsPage />} />
               <Route path="admin/bookings" element={<AdminBookingsPanel />} />
+              <Route path="admin/payments/:bookingRef" element={<PaymentGatewayPage />} />
               <Route path="admin/excursion-bookings" element={<AdminExcursionBookingsPanel />} />
 
 
